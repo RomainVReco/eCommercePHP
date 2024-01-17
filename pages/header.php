ecc15c9af728548f15c47c19104912f25c9f204d
RomainV <romain.vorler@gmail.com>
2024-01-17 11:30:39 +0100

    réorganisation fichiers

---
+++ pages/header.php
@@ -0,0 +1,122 @@
+<div class="container-entete">
+      <div class="upper-zone">
+        <div class="upper-zone-content">
+          <div class="zone-1-upper">
+            <a href="/nouscontacter.html">Nous contacter</a>
+            <a href="/a_propos.html">A propos</a>
+          </div>
+          <div class="zone-1-lower">
+            <button id="button-open" class="style-button-open">
+              <img src="/ressources_entete/bx-menu.svg" alt="menu">
+            </button>
+            <div class="container-menu" id="to-close-menu">
+              <div class="container-menu-div">
+                <div class="h3-close-menu">
+                  <h3>Menu</h3>
+                  <button id="button-close" class="style-button-close"><img src="/ressources_entete/bx-x-36px.svg"
+                      alt="close-icon"></button>
+                  <!-- <input type="checkbox" name="check-close" id="close" onclick="close()"> -->
+                </div>
+                <div class="separator"></div>
+                <div class="bloc-menu">
+                  <div class="choix-menu">
+                    <ul class="liste-menu">
+                      <li><a href="/pages_liste_produits/liste_produits_1.html">Tous nos produits</a></li>
+                      <li><a href="/pages_liste_produits/liste_guitares_acoustiques.html">Guitare Acoustique</a></li>
+                      <li><a href="/pages_liste_produits/liste_guitares_electriques.html">Guitare Electrique</a></li>
+                      <li><a href="/pages_liste_produits/liste_basses.html">Basse</a></li>
+                      <li><a href="/pages_liste_produits/liste_amplis.html">Ampli</a></li>
+                      <li><a href="/pages_liste_produits/liste_effets.html">Effets</a></li>
+                      <li><a href="/pages_liste_produits/liste_accessoires.html">Accessoires</a></li>
+                    </ul>
+                  </div>
+                </div>
+                <h3>Services</h3>
+                <div class="separator"></div>
+                <div class="bloc-menu">
+                  <div class="choix-menu">
+                    <ul class="liste-menu">
+                      <li><a href="/nouscontacter.html">Nous contacter</a></li>
+                      <li><a href="#">Questions fréquentes</a></li>
+                      <li><a href="#">Frais de port et de livraison</a></li>
+                      <li><a href="#">Retourner un produit</a></li>
+                    </ul>
+                  </div>
+                </div>
+                <h3>A propos</h3>
+                <div class="separator"></div>
+                <div class="bloc-menu">
+                  <div class="choix-menu">
+                    <ul class="liste-menu">
+                      <li><a href="/a_propos.html">Informations Guithub</a></li>
+                      <li><a href="#">Conditions générales</a></li>
+                      <li><a href="#">L'univers Guithub</a></li>
+                      <li><a href="https://www.youtube.com/watch?v=86scX2s7hoQ">Tuto make-up</a></li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <a href="/index.html#nouveautes" class="link-zone-1-lower">Nouveautés</a>
+            <a href="/index.html#best_sellers" class="link-zone-1-lower">Best-seller</a>
+          </div>
+          <div class="zone-2">
+            <div class="zone-2-upper">
+              <div class="bloc-image-entete">
+                <a href="/index.html">
+                  <img src="/images/logo_guit_hub_long.png" alt="" class="img-logo">
+                </a>
+              </div>
+            </div>
+            <div class="zone-2-lower">
+              <form action="" class="champ-recherche">
+                <button type="submit"><i class='bx bx-search'></i></button>
+                <input type="text" class="barre-recherche" placeholder="Rechercher">
+              </form>
+            </div>
+          </div>
+          <div class="zone-3-upper">
+            <a href="#">Politique de retour</a>
+            <a href="#">TVA réduite</a>
+          </div>
+          <div class="zone-3-lower">
+            <div class="img-entete">
+              <a href="#"><img src="/ressources_entete/bxs-flag-alt.svg" alt="langue"></a>
+            </div>
+            <div class="img-entete">
+              <a href="/page_login.html"><img src="/ressources_entete/bx-user-circle.svg" alt="langue"></a>
+            </div>
+            <div class="img-entete">
+              <a href="/pages_produits/panier.html"><img src="/ressources_entete/bx-cart.svg" alt="langue"></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="bottom-zone">
+        <nav class="menu-nav">
+          <ol class="list-nav">
+            <a class="categorie_nav" href="pages_liste_produits/liste_produits_1.html">
+              <li>Tous nos produits</li>
+            </a>
+            <a class="categorie_nav" href="pages_liste_produits/liste_guitares_acoustiques.html">
+              <li>Guitares Acoustiques</li>
+            </a>
+            <a class="categorie_nav" href="pages_liste_produits/liste_guitares_electriques.html">
+              <li>Guitares Electriques</li>
+            </a>
+            <a class="categorie_nav" href="pages_liste_produits/liste_basses.html">
+              <li>Basses</li>
+            </a>
+            <a class="categorie_nav" href="pages_liste_produits/liste_amplis.html">
+              <li>Amplis</li>
+            </a>
+            <a class="categorie_nav" href="pages_liste_produits/liste_effets.html">
+              <li>Effets</li>
+            </a>
+            <a class="categorie_nav" href="pages_liste_produits/liste_accessoires.html">
+              <li>Accessoires</li>
+            </a>
+          </ol>
+        </nav>
+      </div>
+    </div>
