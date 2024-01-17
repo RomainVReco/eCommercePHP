29caef30b602754291a24843dfd9767f1eeba98f
RomainV <romain.vorler@gmail.com>
2024-01-17 15:09:43 +0100

    branchement BDD

--- Ressources/BDD/maquettisme.sql
+++ Ressources/BDD/maquettisme.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : mer. 17 jan. 2024 à 11:26
+-- Généré le : mer. 17 jan. 2024 à 14:56
 -- Version du serveur : 10.4.28-MariaDB
 -- Version de PHP : 8.2.4
 
@@ -29,8 +29,21 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `categories` (
   `id_categorie` int(11) NOT NULL,
-  `nom_categorie` int(11) NOT NULL
+  `nom_categorie` varchar(30) NOT NULL,
+  `image_categorie` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `categories`
+--
+
+INSERT INTO `categories` (`id_categorie`, `nom_categorie`, `image_categorie`) VALUES
+(1, 'Maquette militaire', NULL),
+(2, 'Maquette moto', NULL),
+(3, 'Maquette voiture', NULL),
+(4, 'Peinture XF', NULL),
+(5, 'Peinture', NULL),
+(6, 'Pinceaux', NULL);
 
 -- --------------------------------------------------------
 
@@ -77,6 +90,22 @@ CREATE TABLE `marques` (
   `id_pays` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Déchargement des données de la table `marques`
+--
+
+INSERT INTO `marques` (`id_marque`, `nom_marque`, `id_pays`) VALUES
+(1, 'Tamiya', 1),
+(2, 'Revell', 2),
+(3, 'Trumpeter', 3),
+(4, 'Hobby Boss', 4),
+(5, 'Heller', 5),
+(6, 'Italeri', 6),
+(7, 'Zvezda', 7),
+(8, 'Corel', 8),
+(9, 'Ace', 9),
+(10, 'Esprit Maquette', 10);
+
 -- --------------------------------------------------------
 
 --
@@ -85,9 +114,42 @@ CREATE TABLE `marques` (
 
 CREATE TABLE `pays` (
   `id_pays` int(11) NOT NULL,
-  `nom_pays` int(11) NOT NULL,
+  `nom_pays` varchar(50) NOT NULL,
   `parcelable` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `pays`
+--
+
+INSERT INTO `pays` (`id_pays`, `nom_pays`, `parcelable`) VALUES
+(1, 'Austria', 1),
+(2, 'Belgium', 1),
+(3, 'Bulgaria', 1),
+(4, 'Canada', 0),
+(5, 'Croatia', 1),
+(6, 'Cyprus', 1),
+(7, 'Czech Republic', 1),
+(8, 'Denmark', 1),
+(9, 'Estonia', 1),
+(10, 'Finland', 1),
+(11, 'France', 1),
+(12, 'Germany', 1),
+(13, 'Greece', 1),
+(14, 'Hungary', 1),
+(15, 'Ireland', 1),
+(16, 'Italy', 1),
+(17, 'Latvia', 1),
+(18, 'Lithuania', 1),
+(19, 'Luxembourg', 1),
+(20, 'Malta', 1),
+(21, 'Netherlands', 1),
+(22, 'Poland', 1),
+(23, 'Portugal', 1),
+(24, 'Romania', 1),
+(25, 'Slovakia', 1),
+(26, 'Slovenia', 1),
+(27, 'Spain', 1);
 
 -- --------------------------------------------------------
 
@@ -101,12 +163,22 @@ CREATE TABLE `produits` (
   `echelle` varchar(10) DEFAULT NULL,
   `id_categorie` int(11) NOT NULL,
   `quantite` int(11) NOT NULL,
-  `prix` float(8,2) NOT NULL,
+  `prix` decimal(8,2) NOT NULL,
   `id_marque` int(11) NOT NULL,
   `description` text NOT NULL,
   `age_recommande` varchar(4) NOT NULL,
   `reference_image` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `produits`
+--
+
+INSERT INTO `produits` (`id_produit`, `nom_produit`, `echelle`, `id_categorie`, `quantite`, `prix`, `id_marque`, `description`, `age_recommande`, `reference_image`) VALUES
+(10, 'Product 2', 'Small', 2, 50, 29.99, 1, 'A description for Product 2.', '3-8', 'product2.jpg'),
+(11, 'Product 3', NULL, 3, 75, 19.99, 3, 'A description for Product 3.', '10+', NULL),
+(12, 'Product 4', NULL, 1, 4, 79.99, 2, 'A description for Product 4.', '16+', 'product4.jpg'),
+(13, 'Jambon', '1/35', 5, 15, 9.99, 3, 'Description Jambon', '9+', '[value-9].jpg');
 
 -- --------------------------------------------------------
 
@@ -140,7 +212,9 @@ ALTER TABLE `clients`
 -- Index pour la table `commentaires`
 --
 ALTER TABLE `commentaires`
-  ADD PRIMARY KEY (`id_commentaire`);
+  ADD PRIMARY KEY (`id_commentaire`),
+  ADD KEY `id_auteur` (`id_auteur`),
+  ADD KEY `id_produit` (`id_produit`);
 
 --
 -- Index pour la table `marques`
@@ -159,7 +233,9 @@ ALTER TABLE `pays`
 --
 ALTER TABLE `produits`
   ADD PRIMARY KEY (`id_produit`),
-  ADD UNIQUE KEY `reference_image` (`reference_image`);
+  ADD UNIQUE KEY `reference_image` (`reference_image`),
+  ADD KEY `id_categorie` (`id_categorie`),
+  ADD KEY `id_marque` (`id_marque`);
 
 --
 -- Index pour la table `roles`
@@ -187,19 +263,43 @@ ALTER TABLE `commentaires`
 -- AUTO_INCREMENT pour la table `marques`
 --
 ALTER TABLE `marques`
-  MODIFY `id_marque` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_marque` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT pour la table `pays`
+--
+ALTER TABLE `pays`
+  MODIFY `id_pays` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT pour la table `produits`
 --
 ALTER TABLE `produits`
-  MODIFY `id_produit` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_produit` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT pour la table `roles`
 --
 ALTER TABLE `roles`
   MODIFY `id_role` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `commentaires`
+--
+ALTER TABLE `commentaires`
+  ADD CONSTRAINT `commentaires_ibfk_1` FOREIGN KEY (`id_auteur`) REFERENCES `clients` (`id_client`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  ADD CONSTRAINT `commentaires_ibfk_2` FOREIGN KEY (`id_produit`) REFERENCES `produits` (`id_produit`) ON DELETE NO ACTION ON UPDATE NO ACTION;
+
+--
+-- Contraintes pour la table `produits`
+--
+ALTER TABLE `produits`
+  ADD CONSTRAINT `produits_ibfk_1` FOREIGN KEY (`id_categorie`) REFERENCES `categories` (`id_categorie`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `produits_ibfk_2` FOREIGN KEY (`id_marque`) REFERENCES `marques` (`id_marque`) ON DELETE NO ACTION ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- pages/index.php
+++ pages/index.php
@@ -1,3 +1,16 @@
+<?php
+try {
+    $dbh = new PDO('mysql:host=127.0.0.1;dbname=maquettisme;port=3306;charset=utf8mb4', 'root', '');
+    $stmt = $dbh->query('SELECT * FROM categories');
+    $les_categories = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $dbh = null;
+    var_dump($les_categories).PHP_EOL;
+} catch (Exception $e) {
+    $message = $e->getMessage();
+    echo ''. $message .'';
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,6 +25,15 @@
         <?php require_once(__DIR__ . '/header.php'); ?>
     </header>
     <div class="space"></div>
+    <h2>PRINCIPAUX PRODUITS</h2>
+    <?php echo "Nombre de catégories récupérées : ". count($les_categories) . PHP_EOL;
+    ?>
+    <div class="container-category">
+        <div class="main-category">Je suis main</div>
+        <div class="simple-category">je suis simple</div>
+        <div class="simple-category">je suis simple</div>
+        <div class="other-category">je suis main</div>
+    </div>
     <footer>
         <?php require_once(__DIR__ . '/footer.php'); ?>
     </footer>
