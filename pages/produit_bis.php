b47310bd0b334bcc60667993c3b3342499c7a619
Philippe <138588997+Lotratemi@users.noreply.github.com>
2024-01-18 14:23:41 +0100

    Mise en forme image-texte

--- pages/produit_bis.php
+++ pages/produit_bis.php
@@ -1,9 +1,41 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <title>Afficher un champ spécifique depuis une requête SQL</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ma Quête de Rêve</title>
+    <link rel="stylesheet" href="page_produit.css">
 </head>
 <body>
-    <h1>Contenu d'un champ spécifique</h1>
+    <h1>Informations sur l'article</h1>
+        <div class=container>
+                <img itemprop="image" loading="lazy" src="https://cdn.simba-dickie-group.de/media_new/shop-tamiya/products/300024362/00/detail_desktop/1-24-subaru-brz-td8-300024362-fr_00.jpeg?v=1654855948" title="1:24 Subaru BRZ (TD8)" alt="1:24 Subaru BRZ (TD8)" />                        
+                <?php
+                try {
+                    $dbh = new PDO('mysql:host=127.0.0.1;dbname=maquettisme;port=3306;charset=utf8mb4', 'root', '');
+                    $stmt = $dbh->query('SELECT * FROM produits WHERE id_produit = "13"');
+                    $row = $stmt->fetch(PDO::FETCH_ASSOC); // Récupère la première ligne du résultat
+                    
+                    if ($row) {
+                        // Afficher le contenu du champ spécifique (par exemple, le champ 'nom')
+                        echo "<p>Nom article : " . $row['nom_produit'] . "</p>";
+                        echo "<p>Référence article : " . $row['id_produit'] . "</p>";
+                        echo "<p>Prix : " . $row['prix'] . "</p>";
+                    } else {
+                        echo "Aucun résultat trouvé pour cet ID de produit.";
+                    }
+                    
+                    $dbh = null;
+                } catch (Exception $e) {
+                    $message = $e->getMessage();
+                    echo ''. $message .'';
+                }
+                ?>
+                <img itemprop="image" loading="lazy" src="https://cdn.simba-dickie-group.de/media_new/shop-tamiya/products/300024362/00/detail_desktop/1-24-subaru-brz-td8-300024362-fr_00.jpeg?v=1654855948" title="1:24 Subaru BRZ (TD8)" alt="1:24 Subaru BRZ (TD8)" />                        
+                <img itemprop="image" loading="lazy" src="https://cdn.simba-dickie-group.de/media_new/shop-tamiya/products/300024362/00/detail_desktop/1-24-subaru-brz-td8-300024362-fr_00.jpeg?v=1654855948" title="1:24 Subaru BRZ (TD8)" alt="1:24 Subaru BRZ (TD8)" />                                    
+                <img itemprop="image" loading="lazy" src="https://cdn.simba-dickie-group.de/media_new/shop-tamiya/products/300024362/00/detail_desktop/1-24-subaru-brz-td8-300024362-fr_00.jpeg?v=1654855948" title="1:24 Subaru BRZ (TD8)" alt="1:24 Subaru BRZ (TD8)" />                        
+            </div>
+        </div>
     
     <?php
     try {
@@ -13,9 +45,9 @@
         
         if ($row) {
             // Afficher le contenu du champ spécifique (par exemple, le champ 'nom')
-            echo "<p>Contenu du champ 'nom_produit : " . $row['nom_produit'] . "</p>";
-            echo "<p>Contenu du champ 'id_produit : " . $row['id_produit'] . "</p>";
-            echo "<p>Contenu du champ 'prix : " . $row['prix'] . "</p>";
+            echo "<p>Nom article : " . $row['nom_produit'] . "</p>";
+            echo "<p>Référence article : " . $row['id_produit'] . "</p>";
+            echo "<p>Prix : " . $row['prix'] . "</p>";
         } else {
             echo "Aucun résultat trouvé pour cet ID de produit.";
         }
--- pages/produit_quart.php
+++ pages/produit_quart.php
@@ -3,43 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Ma Quête de Rêve</title>
 </head>
 <body>
 <div class="product_detail__info">
         <div class="container">
             <div class="row">
                 <div class="col-md-10 col-xl-6 product_detail__image">
-                    <div class="product_detail__image_wrap js--product_detail__image_wrap">
-                                                <div class="js--container_to_insert">
-                            <img itemprop="image" loading="lazy" src="https://cdn.simba-dickie-group.de/media_new/shop-tamiya/products/300024362/00/detail_desktop/1-24-subaru-brz-td8-300024362-fr_00.jpeg?v=1654855948" title="1:24 Subaru BRZ (TD8)" alt="1:24 Subaru BRZ (TD8)" />                        </div>
-                        
-                        
-                        
-                                            </div>
+                    <img itemprop="image" loading="lazy" src="https://cdn.simba-dickie-group.de/media_new/shop-tamiya/products/300024362/00/detail_desktop/1-24-subaru-brz-td8-300024362-fr_00.jpeg?v=1654855948" title="1:24 Subaru BRZ (TD8)" alt="1:24 Subaru BRZ (TD8)" />                        
                 </div>
+
 
                 <div class="col-12 col-xl-5">
                     <div class="row">
                         <div class="col-10 col-md-12 offset-1 offset-md-0 product_detail--flex">
-                            <!-- image zoom -->
-                            <div id="product_detail_zoom_area"><span></span></div>
-
                             <div class="flex-lg-shrink-1">
                                 <div class="d-flex justify-content-between align-items-start" style="gap: 1rem">
-                                    <h1 class="product_detail__title">1:24 Subaru BRZ (TD8)</h1>
-                                                                    </div>
-                                <div class="product_detail__justify">
-                                    <a href="#reviews">
-                                        <div class="stars js--stars" data-value="5" data-readonly="true"></div>
-                                    </a>
-                                    (2)
+                                    <h1>1:24 Subaru BRZ (TD8)</h1>
                                 </div>
                                 <ul class="product_detail__list">
-                                                                        <li>Référence article : 300024362</li>
-
-                                                                            <li>Âge recommandé : sur 14 ans</li>
-                                                                    </ul>
+                                    <li>Référence article : 300024362</li>
+                                    <li>Âge recommandé : sur 14 ans</li>
+                                </ul>
 
                                                             </div>
                                                             <div class="row flex-xl-row product_detail--bottom">
@@ -61,114 +46,10 @@
                                                 </div>
                                                                                     </div>
                                     </div>
-									<div class="col-12 col-md-6 col-xl-7 cta-button-container">
-                                                                                                                                                                                                                            <a
-                                                        href="/tamiya_fr/mon-compte/ma-liste-de-souhaits/add/30022"
-                                                        rel="nofollow"
-                                                        style="border: 1px solid #ddd;"
-                                                        class="button button--transparent button--filter  "
-                                                >
-                                                    <div class="btn-value step-1">
-                                                        <div class="d-flex flex-fill align-items-center" style="color:#333 !important;">
-                                                            <svg style="fill:#888" class="mr-1">
-                                                                <use xlink:href="/build/img/symbol_sprite.svg?cb=1702302794#heart"></use>
-                                                            </svg>
-                                                            Sur la liste de souhaits                                                        </div>
-                                                    </div>
-                                                    <div class="btn-value step-2">
-                                                        <div class="d-flex flex-fill align-items-center" style="color:#333 !important;">
-                                                            <svg class="anim-spinning mr-1">
-                                                                <use xlink:href="/build/img/symbol_sprite.svg?cb=1702302794#load"></use>
-                                                            </svg>
-                                                            Veuillez patienter                                                        </div>
-                                                    </div>
-                                                    <div class="btn-value step-3">
-                                                        <div class="d-flex flex-fill align-items-center" style="color:#333 !important;">
-                                                            <svg class="mr-1" viewBox="0 0 64 64">
-                                                                <circle class="circle" cx="32" cy="32" r="32" fill="none"/>
-                                                                <path class="check" d="M20 32l9 9 16-16" fill="none" stroke="#f00" stroke-width="4" stroke-linecap="round"/>
-                                                            </svg>
-                                                            Ajouté                                                        </div>
-                                                    </div>
-                                                </a>
-                                                												    <a href="#" class="button" style="display: none"></a>
-                                                                                                                                        												                                                    <div
-                                                            class="vue-3-wrapper vue-3-tw-app vue-3 vue-tw"
-                                                            data-uen-availability-product='%7B%22product%22%3A%7B%22sku%22%3A%22300024362%22%2C%22uid%22%3A%2230022%22%2C%22name%22%3A%221%3A24%20Subaru%20BRZ%20%28TD8%29%22%2C%22img%22%3A%22https%3A%5C%2F%5C%2Fmiddleware.simba-dickie.com%5C%2Fmedia%5C%2Fsdv-availability%5C%2Fproducts%5C%2F300024362%5C%2F00%5C%2Fdetail_desktop%5C%2Favailability.jpeg%22%2C%22price%22%3A41.99%2C%22suffix%22%3A%22%22%2C%22prefix%22%3A%22%26euro%3B%22%2C%22brand%22%3A%22Tamiya%22%7D%2C%22user%22%3A%7B%22logged_in%22%3Afalse%2C%22customer_id%22%3A-1%2C%22customer_mail%22%3A%22%22%7D%2C%22checkout%22%3A%7B%22link%22%3A%22%5C%2Ftamiya_fr%5C%2Fpanier%5C%2F%22%7D%2C%22manage%22%3A%22%5C%2Ftamiya_fr%5C%2Fmon-compte%5C%2Fmes-alertes-de-disponibilite%5C%2F%22%2C%22lang%22%3A%22fr%22%2C%22has_reminder%22%3Afalse%2C%22pwlink%22%3A%22%5C%2Ftamiya_fr%5C%2Fmon-compte%5C%2Fmot-de-passe-oublie%5C%2F%22%2C%22gcaptcha%22%3A%226LfAJe0UAAAAAPk4Zq3Hlwo73DbRob1UL-6vir3l%22%2C%22shop_id%22%3A%2215%22%2C%22main%22%3Atrue%2C%22enc%22%3A%22FzXyYdfEhLvTBcUrNdOeB2xMUXUzS0hJc3dUdjZMTjVEaVUxdzErcGliOXIwdTc0REJ4NWpRVjBSMjdGcUxJY1V3QUJRdm1UWFF1aWJZcEkxMC9nZEhPTlFwSmdNTlVCMzhmQ290UitTMk1mck1CZ2N4enoyR2VsWHE0QWJhQjZUVzljakk0Z09jMmVkK1hEMVlncVJkU3gzOU16RnB5NE9zQ2JnNTZiZ3VMOTdtQkoyNWY3U01EanJHTlRyaGl5VWptU2VZWXBaUjZiTlRpMVdUcG83WUZNOEVoK2picXBSYTlBbHZoWmJWVXdxemRoVDJHeTFuQWc3VHBpeGJiOU5VYnVLNVBQNkt2SGVHOG02M0diaXpEVElCaWVwd1kwa0R3OGF1SGl1eG9WeVpjdjNLRVJOelRaTEhmQXhHd0lFSTFOU2x6eCtMNndqVW5TT1FSMFVVZkZUR3FIVmNhUDgyY2Y4bmhMQmtZM0ZwMk1odUY2QlpKTy8vcjNvQmJLdEhnTnhNUWFqajhneWdvNTVDWnhoM054eVBXOEV4RFlSZXFLc3lvMDYxN0djQ2Jjck9YRE1KZXUxN2pBUVQzOEppTHEzS1htcm93c2FoNm0zTDdwblVuSlA2bXBuT2Fua2NObnVwS25DR2Q4WmRaTDlDaXZIem51U1l6M0YvOTZrSVcxY1RGTE11Sk1xUllUOWhBNkpSWFRXdGN4ak0wVXQ5SERKWW0wOStFN3EwMGZYUXpCRFRmVlpUVnNjMjhmcWRsRkltVjRObkVkT2NROU9wOFdVeC83NU5rdE9mR2NFY1FWRDVVK01YbDAyMXNydytxTkU3ZnI2VHBoaGREYzVZRFFHMWRMeERMMmVoTlpGQ0ZQZFBzV0hSMTFwMm5PQVlwWEFPdTF1VUhoMVUvTnFMWW5VeUV3eitWVStRckd0UmlOVmR4YkhLRFh6RnIzcTVRNGNUa2JKMlRJOVBMQ3BUSXhSVjJ1SmNIN0VwNjBObVFOcGNmRzBTRGJEY1dYUUdGRnZGbEswRWJ5by84LzRCSjJsSTA0NzB2VzloZzBZR0RldnJPVkxBRUZWTm5NSU1lbVRZYmNZUERJNlUvTmZJN2d0bGdDT1BYNWhPcjc5RFFZN2FTODFYNVFOc0ZNNUlQQ0F2eWs4dDBnbnh1MCsxVjNKVFlUMlVXdy9mKzc5NnlxL2hQdUZUbXZRY0U4WDVQT09heDNJNmdhUFJ6K2hTRWhQSURuTkJDdVQ5Um4xL0pKTFBvbW1IZ25kTm1QMjN0VVhodnRDeXp4M0lzKzhFT2hadVlKb1QzM0tQZ21Pc1NBbWt1THY5ZzZPeHhNRHNyd1hMTkdEbUx0cy8zaHZaeE5rRjl6dkVobXVLY2tCcTJsV2k0STBKcm1jbGJEeFhVVTVUWE5ZZUMyUnJrUzNqaVhoaWtpdEw0PQ%3D%3D%22%7D'
-                                                    >
-                                                            <div class="vue-sdg-add-to-cart">
-                                                                <div class="badbrowser" style="width: 100%; text-align: center; padding: 10px;">
-                                                                    Your Browser is not supported or Javascript is disabled.                                                            </div>
-                                                        </div>
-                                                    </div>
-																							                                                                            </div>
-                                </div>
-                                                    </div>
 
-                        <!-- sale action -->
-                                                <!-- /sale action -->
-
-                        <div class="av_flex_helper ">
-                            <div class="col-10 col-md-12 offset-1 offset-md-0">
-    <div class="product_detail__attention   product_detail__attention--first">
-        <div class="product_detail__attention_col product_detail__attention_col--first">
-            <span></span>
-        </div>
-        <div class="product_detail__attention_col">
-            <p>
-                délai de livraison en Allemagne de 1 à 3 jours ouvrables
-            </p>
-        </div>
-    </div>
 </div>
-                                                                                                                            <div class="col-10 col-md-12 offset-1 offset-md-0">
-    <div class="product_detail__attention danger product_detail__attention--last line">
-        <div class="product_detail__attention_col product_detail__attention_col--first">
-            <span>!</span>
-        </div>
-        <div class="product_detail__attention_col allowed-country-list ">
-            <p style="max-width:unset">
-                <strong style="font-weight: 500">Cet article ne peut être livré que dans les pays suivants:</strong><br>
-                Allemagne, Autriche, Portugal, Pays-Bas, Hongrie
-            </p>
-        </div>
-    </div>
 
-    <div class="expand-allowed-country-list ">
-        <i class="fas fa-angle-double-down"></i>
-        <i class="fas fa-angle-double-up"></i>
-    </div>
 </div>                                                                                                                    <style type="text/css">
-                                .av_flex_helper {
-                                    display: flex;
-                                    flex-direction: row;
-                                    flex-wrap: wrap;
-                                    justify-content: flex-start;
-                                    width: 100%;
-                                }
 
-								.av_flex_helper.av_has_free_shipping {
-									margin-top: -20px;
-								}
-                                @media all and (min-width: 768px) {
-                                    .av_flex_helper > div.vue_data_container {
-                                        order: -1;
-                                        margin-top: -58.5px;
-                                        margin-bottom: -7px;
-                                    }
-                                }
-								@media all and (min-width: 1200px) {
-									.av_flex_helper.av_has_free_shipping {
-										margin-top: -33px;
-									}
-								}
-                            </style>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
 </body>
 </html>
