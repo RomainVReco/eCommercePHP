f0d05ba6dea7dc0c44baa8e1792c7c813cfec916
RomainV <romain.vorler@gmail.com>
2024-01-24 12:59:03 +0100

    Commentaires OK

--- Connexion/config.php
+++ Connexion/config.php
@@ -1,11 +1,16 @@
 <?php
+/**
+ * Constantes utilisées pour la gestion des répertoires d'images
+ */
 define("DIR_IMG_PRODUIT", "{$_SERVER["DOCUMENT_ROOT"]}/Lotra3/Ressources/assets_produits/"); 
 define("DIR_IMG_CATEGORIE","{$_SERVER["DOCUMENT_ROOT"]}/Lotra3/Ressources/assets_categories/");
 
 require_once("db_config_access.php");
 
+/**
+ * Représente un produit avec ses caractéristiues essentielles 
+ */
 class Produit {
-
     function __construct($nom, $id, $quantite, $prix, $illustration){
         $this->nom = $nom;
         $this->id = $id;
@@ -23,47 +28,40 @@ class Produit {
     public function getNom(): string {
         return $this->nom;
     }
-
     public function getId(): int {
         return $this->id;
     }
-
     public function getQuantite(): int {
         return $this->quantite;
     }
-
     public function getPrix():float {
         return $this->prix;
     }
-
     public function getImage():string {
         return $this->image;
     }
-
     public function setQuantite($nombre) {
         $this->quantite = $nombre;
     }
-
     public function getTotalProduit() {
         return $this->quantite*$this->prix;
     }
-
     public function ajouterQuantite($adding) {  
         $this->quantite += $adding;
     }
-
 }
 
+
+/**
+ * Représente un panier, contenant une liste de produits
+ */
 class Panier {
 
-    public int $nb;
     public $contenu_panier = [];
     public bool $hasBeenOrdered = false;
 
     function __construct() {
-        $this->nb = 1 ;
     }
-
 
     public function totalPanier(): float {    
         $total = 0;
@@ -74,19 +72,15 @@ class Panier {
         }
         return $total;
     }
-
     public function addItemtoPanier($item){
         $this->contenu_panier[] = $item;
     }
-
     public function getContenuPanier(): array {
         return $this->contenu_panier;
     }
-
     public function validatePanier(){
         $this->hasBeenOrdered = true;
     }
-
     public function getHasBeenOrdered() {
         return $this->hasBeenOrdered;
     }
