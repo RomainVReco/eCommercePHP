96012b8b588a50a69599ad554acf65e342f907bc
Philippe <138588997+Lotratemi@users.noreply.github.com>
2024-01-30 23:00:07 +0100

    amélioration logout

--- pages/logout.php
+++ pages/logout.php
@@ -1,25 +1,13 @@
-<?php  
-  session_destroy();
+<?php
+try {
+    session_start();
+    session_destroy();
+    
+    // Rediriger vers la page index.php
+    header("Location: index.php");
+    exit;
+} catch (Exception $e) {
+    $message = $e->getMessage();
+    echo ''. $message .'';
+}
 ?>
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ma Quête de Rêve</title>
-    <?php require_once(__DIR__ . '/importCSS.php'); ?>
-    <link rel="stylesheet" type="text/css" href="../css/login.css"> 
-</head>
-<body>
-    <header>
-        <?php require_once(__DIR__ . '/header.php'); ?>
-    </header>
-    <div class=partie-haute>
-        <br><br>
-        <h2>Vous avez été déconnecté!!!</h2>
-    </div>
-    <footer>
-        <?php require_once(__DIR__ . '/footer.php'); ?>
-    </footer> 
-</body>
-</html>
